f39e36e11f6cc2ab85c5972a6a84eb0b59b70449
greenlannister <putriauliarahma129@gmail.com>
2025-05-14 03:20:54 +0700

    commit keempat

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,12 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Objek_Wisata;
+use App\Models\Kategori_Wisata;
+use App\Models\Paket_Wisata;
+use App\Models\Diskon;
+use App\Models\Berita;
+
 
 class HomeController extends Controller
 {
@@ -11,9 +17,21 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home.index ', [
-            'title' => 'Home',
+        $obtas = Objek_Wisata::with('kategori_wisata')->get();
+        $kategori_wisatas = Kategori_Wisata::all();
+        $paket_wisatas = Paket_Wisata::all(); // ambil data dari tabel ketiga
+        $diskons = Diskon::all();
+        $beritas = Berita::all();
+
+        return view('home.index', [
+            'obtas' => $obtas,
+            'kategori_wisatas' => $kategori_wisatas,
+            'paket_wisatas' => $paket_wisatas, // kirim ke view
+            'diskons' => $diskons,
+            'beritas' => $beritas,
+            'title' => 'Home'
         ]);
+    
     }
 
     /**
